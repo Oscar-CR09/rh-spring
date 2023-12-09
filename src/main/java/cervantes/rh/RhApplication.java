c9398d89730a704a3ebffaea910d13f8c2b8b2a8
Oscar <oscarcr189@outlook.com>
2023-12-08 20:50:00 -0600

    Proyecto RH 1 inicio

---
+++ src/main/java/cervantes/rh/RhApplication.java
@@ -0,0 +1,14 @@
+package cervantes.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RhApplication.class, args);
+	}
+
+}
