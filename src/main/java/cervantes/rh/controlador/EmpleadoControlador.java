8e3118614627b180e14442cd7ff2060c716f467a
Oscar <oscarcr189@outlook.com>
2023-12-13 20:51:25 -0600

    Proyecto RH 5

---
+++ src/main/java/cervantes/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,13 @@
+package cervantes.rh.controlador;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController //http//localhost:8080/rh-app/
+@RequestMapping("rh-app")
+@CrossOrigin
+public class EmpleadoControlador {
+
+}
