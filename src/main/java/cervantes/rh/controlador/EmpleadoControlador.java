d90d45eb7ed8d36f00211f935592322870fc492e
Oscar <oscarcr189@outlook.com>
2023-12-15 20:25:55 -0600

    Proyecto RH 6

--- src/main/java/cervantes/rh/controlador/EmpleadoControlador.java
+++ src/main/java/cervantes/rh/controlador/EmpleadoControlador.java
@@ -1,5 +1,10 @@
 package cervantes.rh.controlador;
 
+import cervantes.rh.servicio.EmpleadoServicio;
+import cervantes.rh.servicio.IEmpleadoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController //http//localhost:8080/rh-app/
 @RequestMapping("rh-app")
-@CrossOrigin
+@CrossOrigin(value = "http://localhost:3000")
 public class EmpleadoControlador {
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
+
+
+    @Autowired
+    private IEmpleadoServicio empleadoServicio;
+
+
+
 
 }
