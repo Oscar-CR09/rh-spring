00310f9dcc4f2cb9d72faede7fcfeedba7279b3c
Oscar <oscarcr189@outlook.com>
2023-12-15 20:46:26 -0600

    Proyecto RH 7

--- src/main/java/cervantes/rh/controlador/EmpleadoControlador.java
+++ src/main/java/cervantes/rh/controlador/EmpleadoControlador.java
@@ -1,13 +1,17 @@
 package cervantes.rh.controlador;
 
+import cervantes.rh.modelo.Empleado;
 import cervantes.rh.servicio.EmpleadoServicio;
 import cervantes.rh.servicio.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController //http//localhost:8080/rh-app/
@@ -20,7 +24,13 @@ public class EmpleadoControlador {
     @Autowired
     private IEmpleadoServicio empleadoServicio;
 
-
+    //http://localhost:8080/rh-app/empleados
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleado(){
+        var empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado -> logger.info(empleado.toString())));
+        return empleados;
+    }
 
 
 }
