894f64a82fb5a6160928b4c47add09fa53636ec0
Oscar <oscarcr189@outlook.com>
2023-12-25 08:55:07 -0600

    Proyecto RH 8

--- src/main/java/cervantes/rh/controlador/EmpleadoControlador.java
+++ src/main/java/cervantes/rh/controlador/EmpleadoControlador.java
@@ -1,15 +1,14 @@
 package cervantes.rh.controlador;
 
+import cervantes.rh.excepcion.RecursoNoEncontradoExcepcion;
 import cervantes.rh.modelo.Empleado;
 import cervantes.rh.servicio.EmpleadoServicio;
 import cervantes.rh.servicio.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +29,24 @@ public class EmpleadoControlador {
         var empleados = empleadoServicio.listarEmpleados();
         empleados.forEach((empleado -> logger.info(empleado.toString())));
         return empleados;
+    }
+
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado){
+        logger.info("Empleado agregar: " +empleado);
+        return empleadoServicio.guardarEmpleado(empleado);
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado>
+    obtenerEmpleadoPorId(@PathVariable Integer id){
+        Empleado empleado =empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null ) {
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+        }
+
+        return ResponseEntity.ok(empleado);
+
     }
 
 
